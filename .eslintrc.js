8eb13fb81422e61d5484798a1e2062ca52565e72
LRNZH <fullmentalfloss@gmail.com>
2023-06-05 14:31:28 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,10 +30,7 @@ module.exports = {
       'error',
       2
     ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
+    'linebreak-style': 'off',
     'quotes': [
       'error',
       'single'
